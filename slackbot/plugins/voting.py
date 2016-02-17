5eb1da1ad721cca8c452a6d0dfecd28caa11fd87
scottlafetra <scottlafetra@gmail.com>
2016-02-17 10:35:39 -0800

    Added voting channel lock and 1 vote per person

--- slackbot/plugins/voting.py
+++ slackbot/plugins/voting.py
@@ -4,59 +4,90 @@ import re
 
 import time
 
+voteName = ""
+
 options = []
 votes = []
 
+voted = {}
+voteChannel = ""
+
 voteCloseTime = 0
 
-@respond_to("start vote", re.IGNORECASE)
-def startVote(message):
+def isVoteOpen(message):
+    global voteCloseTime
+    global voteChannel
+
+    if time.time() >= voteCloseTime:
+        message.reply("No active vote.")
+
+    elif voteChannel != message._body["channel"]:
+            message.reply("Vote locked to another channel.")
+
+    else:
+        return True
+
+    return False
+
+@respond_to("start vote (.*)", re.IGNORECASE)
+def startVote(message, setVoteName):
+    global voteName
     global options
     global votes
     global voteCloseTime
+    global voted
+    global voteChannel
 
     #reset variables
+    voteName = setVoteName
     options = []
     votes = []
     voteCloseTime = time.time() + 60*60 #give an hour to vote
 
-    message.reply("Vote started")
+    voteChannel = message._body["channel"]
+
+    message.reply("Vote \"" + voteName + "\" started")
     
 
 @respond_to("add option (.*)", re.IGNORECASE)
 def addOption(message, optionName):
     global options
     global votes
-    global voteCloseTime
 
-    if time.time() < voteCloseTime:
+    if isVoteOpen(message):
         options.append(optionName)
         votes.append(0)
 
         message.reply("Option \"" + optionName + "\" added.")
 
-@respond_to("list options", re.IGNORECASE)
+@respond_to("list vote", re.IGNORECASE)
 def listOptions(message):
     global options
     global votes
-    global voteCloseTime
+    global voteName
 
-    if time.time() < voteCloseTime:
+    if isVoteOpen(message):
+        message.send(voteName)
         message.send("Options:")
         
         for option in options:
             message.send(option)
 
-@respond_to("vote (.*)", re.IGNORECASE)
+@respond_to("cast vote (.*)", re.IGNORECASE)
 def addVote(message, voteFor):
     global options
     global votes
-    global voteCloseTime
+    global voted
 
-    if time.time() < voteCloseTime:
+    if isVoteOpen(message):
         if options.count(voteFor) > 0:
+            voter = message._get_user_id()
+
+            if voter in voted:#if they already voted
+                votes[options.index(voted[voter])] -= 1 #remove previous vote
             
             votes[options.index(voteFor)] += 1
+            voted[voter] = voteFor #record vote for changing
             message.reply("Sucessfully voted for " + voteFor)
 
         else:
@@ -66,9 +97,8 @@ def addVote(message, voteFor):
 def endVote(message):
     global options
     global votes
-    global voteCloseTime
 
-    if time.time() < voteCloseTime:
+    if isVoteOpen(message):
         voteCloseTime = 0
 
         maxVotes =  0
