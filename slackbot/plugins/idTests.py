f596091dcfa72c8accd30100a5c4bfb3a94ef865
scottlafetra <scottlafetra@gmail.com>
2016-02-17 08:31:39 -0800

    Added test bits

--- slackbot/plugins/idTests.py
+++ slackbot/plugins/idTests.py
@@ -2,3 +2,10 @@ from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 import re
 
+@respond_to("Who am I?", re.IGNORECASE)
+def replyID(message):
+    message.reply("Human #" + message._get_user_id());
+
+@respond_to("Where am I?", re.IGNORECASE)
+def replyChannelID(message):
+    message.reply("Channel #" + message._body['channel']);
