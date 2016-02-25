c9cc56804f4f835898387038685a975475961684
scottlafetra <scottlafetra@gmail.com>
2016-02-24 22:11:00 -0800

    Expanded roar functinoality

--- slackbot/plugins/roar.py
+++ slackbot/plugins/roar.py
@@ -6,3 +6,10 @@ import re
 @respond_to("roar", re.IGNORECASE)
 def roar(message):
     message.reply("ROAR")
+
+@listen_to("rawr", re.IGNORECASE)
+@respond_to("rawr", re.IGNORECASE)
+@listen_to("raor", re.IGNORECASE)
+@respond_to("raor", re.IGNORECASE)
+def roar(message):
+    message.reply("You call that a roar?")
